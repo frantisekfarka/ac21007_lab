a5d3cb372828a74422a666527c4c2b39ef5db611
Chris Warburton <chriswarbo@gmail.com>
2016-01-18 17:09:27 +0000

    Minor comment changes

--- lab01/Main.hs
+++ lab01/Main.hs
@@ -21,20 +21,20 @@ main = putStrLn (hello "World")
 -- Implement your own boolean functions:
 
 
--- | myAnd returns True only if both of the input are True.
+-- | myAnd returns True only if both of the inputs are True.
 --
 -- myAnd :: Bool -> Bool -> Bool,
 
--- | myOr returns False only if both of the input are False.
+-- | myOr returns False only if both of the inputs are False.
 --
 -- myOr :: Bool -> Bool -> Bool
 
 -- | myImply x y should return False only if x is True and y is False, for the
---   other cases it returnsTrue
+--   other cases it returns True
 --
 -- myImply :: Bool -> Bool -> Bool
 
--- | myXor is called Exclusive disjunction(exclusive or), is a logical operation
+-- | myXor is called Exclusive disjunction (exclusive or), is a logical operation
 --   that outputs True whenever both inputs differ (one is True, the other is False)
 --
 -- myXor :: Bool -> Bool -> Bool.
